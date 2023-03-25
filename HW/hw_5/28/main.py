84618aa5403b185c93f2ef2eca15ce0e179c2cb0
ilya_k <ivk68@list.ru>
2023-03-25 13:09:25 +0300

    +

--- HW/hw_5/28/main.py
+++ HW/hw_5/28/main.py
@@ -10,5 +10,5 @@ def summ(a, b):
     if b == 0:
         return a
     return summ(a + 1, b - 1)
-summa = summ(int(input('a = ')), int(input('b= ')))
-print(f'     {summa}')
+summa = summ(int(input('a = ')), int(input('b = ')))
+print(f'    {summa}')
