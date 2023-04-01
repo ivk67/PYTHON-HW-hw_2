61bc2528b9ecd35eef5f514adabc93b8e44e6eb3
ilya_k <ivk68@list.ru>
2023-04-01 22:03:25 +0300

    +

--- HW/hw_7/34/main.py
+++ HW/hw_7/34/main.py
@@ -10,7 +10,7 @@
 
 c = input('введите фразу: ')
 st = c.lower().split()
-f = lambda x: sum(1 for i in x if i in 'аеёиоуыэюя')
+def f(x): return sum(1 for i in x if i in 'аеёиоуыэюя')
 t = f(st[0])
 if all([f(i) == t for i in st]):
     print('Парам пам-пам')
